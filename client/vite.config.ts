65eb4e9ca0c938b737612318df616a74051a8845
fallenfreq <michael@catsdog.co.uk>
2024-08-30 22:36:48 +0100

    Change client output dir

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -9,6 +9,9 @@ import { PrimeVueResolver } from '@primevue/auto-import-resolver'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: '../api/client_dist'
+  },
   plugins: [
     {
       name: 'postbuild-commands',
