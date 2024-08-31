8a55e7f17bf3de5ffec4140bcf12c76b39f7b8e0
fallenfreq <michaelhigginson@hotmail.co.uk>
2024-08-31 14:46:00 +0100

    Renamed app

--- api/drizzle.config.ts
+++ api/drizzle.config.ts
@@ -5,7 +5,7 @@ export default {
   out: './migrations', // This is where your migration files will be stored
   driver: 'd1', // D1 driver
   dbCredentials: {
-    wranglerConfigPath: '@vue-app/wrangler.toml', // Path to your wrangler.toml file
+    wranglerConfigPath: '@somefreq-app/wrangler.toml', // Path to your wrangler.toml file
     dbName: 'DB' // The binding name for your D1 database as defined in wrangler.toml
   }
 } satisfies Config
