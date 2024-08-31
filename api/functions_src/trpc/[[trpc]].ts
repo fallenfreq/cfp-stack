0025433d6b92542f7619533cbf682b169632c120
fallenfreq <michael@catsdog.co.uk>
2024-08-31 23:15:56 +0100

    Added error handler to functions

--- api/functions_src/trpc/[[trpc]].ts
+++ api/functions_src/trpc/[[trpc]].ts
@@ -5,20 +5,24 @@ import * as schema from '../../dist/schemas/schema.js'
 import { initEnvs, type Envs } from '../../dist/config/envs.js'
 
 export const onRequest: PagesFunction<Envs> = async ({ request, env }) => {
-  initEnvs(env)
+  try {
+    initEnvs(env)
 
-  // Dynamically import any dependencies that use the `env` object
-  const [{ appRouter }] = await Promise.all([import('../../dist/routes/appRouter.js')])
+    // Dynamically import any dependencies that use the `env` object
+    const [{ appRouter }] = await Promise.all([import('../../dist/routes/appRouter.js')])
 
-  return fetchRequestHandler({
-    endpoint: '/trpc',
-    req: request,
-    router: appRouter,
-    createContext: (): Context => {
-      return {
-        req: request,
-        db: drizzle(env.DB, { schema })
+    return fetchRequestHandler({
+      endpoint: '/trpc',
+      req: request,
+      router: appRouter,
+      createContext: (): Context => {
+        return {
+          req: request,
+          db: drizzle(env.DB, { schema })
+        }
       }
-    }
-  })
+    })
+  } catch (error: any) {
+    return new Response(error.toString(), { status: 500 })
+  }
 }
