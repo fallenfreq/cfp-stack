00b3ce632d02dcba756bc311d50239e27f2d834a
fallenfreq <michael@catsdog.co.uk>
2024-09-07 11:20:30 +0100

    Removed flex from main

--- client/src/views/ContactView.vue
+++ client/src/views/ContactView.vue
@@ -1,7 +1,9 @@
 <template>
-  <div>
-    <h1 class="font-bold">Contact</h1>
-    <p>Mobile: 07572377622</p>
-    <p>Email: michael@somefreq.com</p>
+  <div class="flex justify-center">
+    <div>
+      <h1 class="font-bold">Contact</h1>
+      <p>Mobile: 07572377622</p>
+      <p>Email: michael@somefreq.com</p>
+    </div>
   </div>
 </template>
