70c009f9331f0f5bf2795be95b40d9a56e8472a9
fallenfreq <michael@catsdog.co.uk>
2024-09-07 11:23:14 +0100

    Import components

--- client/components.d.ts
+++ client/components.d.ts
@@ -7,12 +7,14 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
+    BasicCard: typeof import('./src/components/BasicCard.vue')['default']
     BasicFooter: typeof import('./src/components/BasicFooter.vue')['default']
     ComingSoon: typeof import('./src/components/ComingSoon.vue')['default']
     DarkModeSwitch: typeof import('./src/components/DarkModeSwitch.vue')['default']
     MiniMothLogo: typeof import('./src/components/brand/MiniMothLogo.vue')['default']
     MothLogo: typeof import('./src/components/brand/MothLogo.vue')['default']
     MothWordmark: typeof import('./src/components/brand/MothWordmark.vue')['default']
+    Portfolio: typeof import('./src/components/Portfolio.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
     VuesticNavBar: typeof import('./src/components/VuesticNavBar.vue')['default']
