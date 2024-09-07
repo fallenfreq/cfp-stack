8dcc18e00cecea4e8193435d36f153509f8a43c6
fallenfreq <michael@catsdog.co.uk>
2024-09-07 11:21:02 +0100

    Added portfolio page layout

--- client/src/views/PortfolioBranding.vue
+++ client/src/views/PortfolioBranding.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class=".flex-grow">
-    <h1>Branding portfolio</h1>
-    <ComingSoon />
+  <div class="mx-5">
+    <h1 class="text-4xl mb-5">Branding portfolio</h1>
+    <Portfolio />
   </div>
 </template>
