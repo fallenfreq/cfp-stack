1568828ab87f6f2215ee1cbe1e73bae8b4449e51
fallenfreq <michaelhigginson@hotmail.co.uk>
2024-09-08 16:08:13 +0100

    Display necessary placeholder only

--- client/src/components/Portfolio.vue
+++ client/src/components/Portfolio.vue
@@ -3,37 +3,140 @@
     class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 2xl:grid-cols-5 gap-5"
   >
     <BasicCard
-      v-for="item in displayedItems"
-      :key="item.id"
+      v-for="(item, index) in displayedItems"
+      :key="index"
       :image="item.image"
       :title="item.title"
       :is-placeholder="item.isPlaceholder"
+      :class="[
+        'portfolio-item',
+        { 'is-placeholder': item.isPlaceholder },
+        ...item.visibilityClasses
+      ]"
     />
   </div>
 </template>
 
-<script setup>
-import { ref, onMounted, computed } from 'vue'
+<script lang="ts" setup>
+import { ref, onMounted } from 'vue'
 import { fetchPortfolioItems } from '@/services/portfolio'
 
-const portfolioItems = ref([])
+// Interface for PortfolioItem type
+interface PortfolioItem {
+  image: string | null
+  title: string
+  isPlaceholder: boolean
+  visibilityClasses?: string[] // New field for visibility classes
+}
+
+interface Breakpoints {
+  default: number
+  sm: number
+  md: number
+  lg: number
+  xl: number
+  '2xl': number
+}
+
+// Define breakpoints and the number of columns for each
+const columns: Breakpoints = {
+  default: 1,
+  sm: 2,
+  md: 3,
+  lg: 4,
+  xl: 4,
+  '2xl': 5
+}
+
+const displayedItems = ref<PortfolioItem[]>([])
+
+const calculatePlaceholdersNeeded = (numItems: number) => {
+  const placeholdersNeeded: Breakpoints = {
+    default: 0,
+    sm: 0,
+    md: 0,
+    lg: 0,
+    xl: 0,
+    '2xl': 0
+  }
+  for (const key of Object.keys(columns)) {
+    const colCount = columns[key as keyof Breakpoints]
+    if (numItems === 0) {
+      // If no items, full line of placeholders
+      placeholdersNeeded[key as keyof Breakpoints] = colCount
+    } else {
+      const remainder = numItems % colCount
+      placeholdersNeeded[key as keyof Breakpoints] = remainder === 0 ? 0 : colCount - remainder
+    }
+  }
+  return placeholdersNeeded
+}
+
+const getMaxItemsCount = (realItemsCount: number, placeholdersNeeded: Breakpoints): number => {
+  const highestPlaceholderCount = Math.max(...Object.values(placeholdersNeeded))
+  return realItemsCount + highestPlaceholderCount
+}
 
 onMounted(async () => {
-  const items = await fetchPortfolioItems()
-  portfolioItems.value = items
-})
+  const items = (await fetchPortfolioItems()) as PortfolioItem[]
+  const placeholdersNeeded = calculatePlaceholdersNeeded(items.length)
 
-const displayedItems = computed(() => {
-  const items = portfolioItems.value
-  const placeholdersNeeded = Math.max(0, 4 - items.length)
+  for (let i = 0; i < getMaxItemsCount(items.length, placeholdersNeeded); i++) {
+    const placeholder: PortfolioItem = {
+      image: null,
+      title: 'Coming Soon!',
+      isPlaceholder: true,
+      visibilityClasses: []
+    }
 
-  const placeholders = Array(placeholdersNeeded).fill({
-    image: null,
-    title: 'Coming Soon!',
-    isPlaceholder: true
-  })
+    const { visibilityClasses } = placeholder
+    for (const key of Object.keys(placeholdersNeeded)) {
+      if (i < placeholdersNeeded[key as keyof Breakpoints])
+        visibilityClasses?.push('visible-' + key)
+    }
 
-  // Combine actual items with placeholders
-  return [...items.map((item) => ({ ...item, isPlaceholder: false })), ...placeholders]
+    displayedItems.value.push(placeholder)
+  }
 })
 </script>
+
+<style scoped>
+/* Hide placeholders by default */
+.is-placeholder {
+  display: none;
+}
+
+.visible-default {
+  display: block;
+}
+
+@media (min-width: 640px) {
+  .visible-sm {
+    display: block;
+  }
+}
+
+@media (min-width: 768px) {
+  .visible-md {
+    display: block;
+  }
+}
+
+@media (min-width: 1024px) {
+  .visible-lg {
+    display: block;
+  }
+}
+
+@media (min-width: 1280px) {
+  .visible-xl {
+    display: block;
+  }
+}
+
+@media (min-width: 1536px) {
+  .visible-2xl {
+    display: block;
+  }
+}
+</style>
