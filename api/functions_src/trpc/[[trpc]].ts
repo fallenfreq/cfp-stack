20d00cb769c9271c5afea59dbb7280875640db72
fallenfreq <michael@catsdog.co.uk>
2024-09-09 15:54:10 +0100

    Added onError handler to tRPC

--- api/functions_src/trpc/[[trpc]].ts
+++ api/functions_src/trpc/[[trpc]].ts
@@ -20,9 +20,26 @@ export const onRequest: PagesFunction<Envs> = async ({ request, env }) => {
           req: request,
           db: drizzle(env.DB, { schema })
         }
+      },
+      onError: ({ error }) => {
+        // Change error to hide deatils from the client and log if required
+        if (error.code == 'INTERNAL_SERVER_ERROR') {
+          // Can log errors from here
+          console.log({
+            name: error.name,
+            code: error.code,
+            message: error.message
+          })
+          // Change error
+          error.stack = ''
+          error.message = 'Internal Server Error'
+        }
+        // throw Error('Will crash if thrown here')
       }
     })
   } catch (error: any) {
-    return new Response(error.toString(), { status: 500 })
+    // Handle errors that occur outside of tRPC
+    console.log(error)
+    return new Response('Internal Server Error: ', { status: 500 })
   }
 }
