fa9660be49ab2660a78266b8b59e84ae2075a4be
fallenfreq <michael@catsdog.co.uk>
2024-09-10 17:37:04 +0100

    Added notes for testing portfolio without a database

--- api/src/routes/portfolio/router.ts
+++ api/src/routes/portfolio/router.ts
@@ -1,6 +1,18 @@
 import { publicProcedure, router } from '../../config/trpc.js'
 import { portfolioEntries } from '../../schemas/portfolio.js'
 
+// return test items or throw to test without having to add items to the database
+// return [
+//   {
+//     id: 1,
+//     description: 'This is a real item',
+//     imageUrl: 'https://placecats.com/400/300',
+//     title: 'Real item',
+//     isPlaceholder: false
+//   }
+// ]
+// throw new Error('not implemented')
+
 export const portfolioRouter = router({
   entry: publicProcedure.query(async (opt) => {
     return opt.ctx.db.select().from(portfolioEntries)
