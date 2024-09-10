670058cd7da469d845cbb5db7ccc9f84bcea243b
fallenfreq <michael@catsdog.co.uk>
2024-09-10 18:00:45 +0100

    Added temporary console.log

--- client/src/config/queryClient.ts
+++ client/src/config/queryClient.ts
@@ -7,6 +7,7 @@ const queryClient = new QueryClient({
   queryCache: new QueryCache({
     onError: (error) => {
       // Duration in milliseconds. Set to 0 to prevent automatic close
+      console.log('Query error', error)
       useToast().notify({
         duration: 10000,
         color: 'danger',
