c346c46178c2cf84e6f199f1c74b49b5ffc9a5bc
fallenfreq <michael@catsdog.co.uk>
2024-09-10 17:36:32 +0100

    Added overflow hidden to continer div of images

--- client/src/components/BasicCard.vue
+++ client/src/components/BasicCard.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="w-full h-auto">
     <div
-      class="bg-backgroundSecondary rounded-lg relative w-full aspect-[4/3] flex items-center justify-center"
+      class="bg-backgroundSecondary rounded-lg overflow-hidden relative w-full aspect-[4/3] flex items-center justify-center"
     >
       <MothLogo v-if="isPlaceholder" class="w-1/2 h-1/2 opacity-5" />
       <img v-else class="w-full h-full object-cover" :src="image" alt="Portfolio Image" />
