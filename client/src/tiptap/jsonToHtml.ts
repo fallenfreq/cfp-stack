7573ca1149d03c5657df80f06a493769e0e5ab88
fallenfreq <michaelhigginson@hotmail.co.uk>
2024-10-11 03:18:44 +0100

    Refactored

--- client/src/tiptap/jsonToHtml.ts
+++ client/src/tiptap/jsonToHtml.ts
@@ -111,15 +111,10 @@ function initGenerateDynamicHTML(editor: Editor) {
       ...dynamicNodes,
       Youtube.extend({
         renderHTML({ node, HTMLAttributes }) {
-          node.type.spec.attrs
-          // Use reduce to filter out default values from HTMLAttributes
-          const filteredAttributes = Object.keys((node.type.spec.attrs ??= {})).reduce(
+          const attrs = (node.type.spec.attrs ??= {})
+          const filteredAttributes = Object.keys(attrs).reduce(
             (acc, key) => {
-              node.type.spec.attrs ??= {}
-              const attrSpec = node.type.spec.attrs[key]
-              const defaultValue = attrSpec.default
-              // Only add the attribute if it does not match the default value
-              if (HTMLAttributes[key] !== defaultValue) {
+              if (HTMLAttributes[key] !== attrs[key].default) {
                 acc[key] = HTMLAttributes[key]
               }
               return acc
