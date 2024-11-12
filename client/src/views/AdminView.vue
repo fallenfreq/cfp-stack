36ed5778eed272801343bd3b240924eab6448886
fallenfreq <michael@catsdog.co.uk>
2024-11-12 01:03:02 +0000

    fixed class

--- client/src/views/AdminView.vue
+++ client/src/views/AdminView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class=".flex-grow">
+  <div class="flex-grow">
     <h1>This is an administrator page.</h1>
     <h2>You can see it because you have the role "admin" in your ZITADEL project.</h2>
   </div>
