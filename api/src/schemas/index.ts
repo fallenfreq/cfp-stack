c11743a7a93c2644b5d78a71c63e19c8002e453f
fallenfreq <michael@catsdog.co.uk>
2024-11-26 00:53:57 +0000

    Merged schemas

---
+++ api/src/schemas/index.ts
@@ -0,0 +1,6 @@
+import * as user from './user.js'
+import * as mapMarker from './mapMarker.js'
+import * as portfolio from './portfolio.js'
+
+const schemas = { ...user, ...portfolio }
+export { schemas }
