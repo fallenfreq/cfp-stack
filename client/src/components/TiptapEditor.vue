7dc725476af555cddd198519875fab4e730cd9f6
fallenfreq <michaelhigginson@hotmail.co.uk>
2025-02-02 17:03:46 +0000

    Drag handle added to blocks in editor

--- client/src/components/TiptapEditor.vue
+++ client/src/components/TiptapEditor.vue
@@ -26,6 +26,7 @@ import { useEditor, EditorContent, VueNodeViewRenderer, type NodeViewProps } fro
 import { registerCustomNodes } from '@/tiptap/registerCustomNodes'
 import { AllowAttributesExtension } from '@/tiptap/allowAttributesExtension'
 import { initGenerateDynamicHTML } from '@/tiptap/jsonToHtml'
+import cssVariables from '../../cssVariables.js'
 
 // import prettier from 'prettier'
 import prettier from 'prettier/standalone'
@@ -42,6 +43,11 @@ import TableHeader from '@tiptap/extension-table-header'
 import TableRow from '@tiptap/extension-table-row'
 import Span from '@/tiptap/spanExtention'
 import Div from '@/tiptap/divExtention'
+
+// Drag handle extension
+// import DropCursor from '@tiptap/extension-dropcursor' // used by GlobalDragHandle already
+import GlobalDragHandle from 'tiptap-extension-global-drag-handle'
+import AutoJoiner from 'tiptap-extension-auto-joiner'
 
 import TiptapCodeBlock from './TiptapCodeBlock.vue'
 import CodeBlockLowlight from '@tiptap/extension-code-block-lowlight'
@@ -193,6 +199,7 @@ const editor = useEditor({
     }).configure({ lowlight }),
     StarterKit.configure({
       codeBlock: false
+      // document: false
     }),
     Youtube.extend({
       renderHTML({ node, HTMLAttributes }) {
@@ -238,7 +245,10 @@ const editor = useEditor({
     Span,
     Div,
     ...registerCustomNodes(),
-    AllowAttributesExtension
+    AllowAttributesExtension,
+    // DropCursor,
+    GlobalDragHandle,
+    AutoJoiner
   ],
   content: initialContent,
   autofocus: true,
@@ -271,9 +281,50 @@ const toggleView = async () => {
   }
   isCodeView.value = !isCodeView.value
 }
+
+let svgUrl = `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 10 10' style='fill: rgba(${cssVariables.root['--primary']} / 1)'%3E%3Cpath d='M3,2 C2.44771525,2 2,1.55228475 2,1 C2,0.44771525 2.44771525,0 3,0 C3.55228475,0 4,0.44771525 4,1 C4,1.55228475 3.55228475,2 3,2 Z M3,6 C2.44771525,6 2,5.55228475 2,5 C2,4.44771525 2.44771525,4 3,4 C3.55228475,4 4,4.44771525 4,5 C4,5.55228475 3.55228475,6 3,6 Z M3,10 C2.44771525,10 2,9.55228475 2,9 C2,8.44771525 2.44771525,8 3,8 C3.55228475,8 4,8.44771525 4,9 C4,9.55228475 3.55228475,10 3,10 Z M7,2 C6.44771525,2 6,1.55228475 6,1 C6,0.44771525 6.44771525,0 7,0 C7.55228475,0 8,0.44771525 8,1 C8,1.55228475 7.55228475,2 7,2 Z M7,6 C6.44771525,6 6,5.55228475 6,5 C6,4.44771525 6.44771525,4 7,4 C7.55228475,4 8,4.44771525 8,5 C8,5.55228475 7.55228475,6 7,6 Z M7,10 C6.44771525,10 6,9.55228475 6,9 C6,8.44771525 6.44771525,8 7,8 C7.55228475,8 8,8.44771525 8,9 C8,9.55228475 7.55228475,10 7,10 Z'%3E%3C/path%3E%3C/svg%3E")`
 </script>
 
 <style>
+.drag-handle {
+  position: fixed;
+  opacity: 1;
+  transition: opacity ease-in 0.2s;
+  border-radius: 0.25rem;
+  background-image: v-bind(svgUrl);
+  background-size: calc(0.5em + 0.375rem) calc(0.5em + 0.375rem);
+  background-repeat: no-repeat;
+  background-position: center;
+  width: 1.2rem;
+  height: 1.5rem;
+  z-index: 50;
+  cursor: grab;
+
+  &:hover {
+    background-color: rgba(var(--textPrimary) / 0.1);
+    transition: opacity 0.2s;
+  }
+
+  &:active {
+    background-color: rgba(var(--textPrimary) / 0.1);
+    transition: opacity 0.2s;
+    cursor: grabbing;
+  }
+
+  &.hide {
+    opacity: 0;
+    pointer-events: none;
+  }
+
+  @media screen and (max-width: 600px) {
+    display: none;
+    pointer-events: none;
+  }
+}
+/* Styling for drop position */
+.ProseMirror-selectednode {
+  outline: 3px solid var(--purple);
+}
 /* Youtube video styling in the editor */
 
 .tiptap [data-youtube-video]:has(> iframe.resp-yt) {
