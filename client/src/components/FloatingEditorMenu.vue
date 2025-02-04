e15b758863f94c70d7e501c8ce6e898a2781271c
fallenfreq <michaelhigginson@hotmail.co.uk>
2025-02-04 00:01:05 +0000

    Refactored ready to implement a floating nav for the editor

---
+++ client/src/components/FloatingEditorMenu.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <VaChip
+      @click="editor.chain().focus().toggleCodeBlock().run()"
+      :class="{ 'is-active': editor.isActive('codeBlock') }"
+    >
+      Code Block
+    </VaChip>
+    <VaChip @click="() => prettifySelectedCode(editor)">Format</VaChip>
+    <VaChip @click="editorStore.toggleCodeView">
+      {{ editorStore.isCodeView ? 'Aa' : `\<\/\>` }}
+    </VaChip>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { prettifySelectedCode } from '@/tiptap/editorUtils'
+import { useEditorStore } from '@/stores/editorStore.js'
+const editorStore = useEditorStore()
+import type { Editor } from '@tiptap/vue-3'
+defineProps<{ editor: Editor }>()
+</script>
+
+<style></style>
