ff69ef64f98b8c804d819ca7006aae67a3300892
fallenfreq <michaelhigginson@hotmail.co.uk>
2025-02-19 21:37:10 +0000

    Improve continuity for users when editing nodeView content

--- client/src/tiptap/customComponentNode.ts
+++ client/src/tiptap/customComponentNode.ts
@@ -6,7 +6,7 @@ import {
   mergeAttributes,
   NodeViewWrapper
 } from '@tiptap/vue-3'
-import { defineComponent, h } from 'vue'
+import { defineComponent, h, onUnmounted, ref } from 'vue'
 import { type ComponentData } from './editorComponents'
 
 // Utility function to create a Tiptap node for Vue components
@@ -44,21 +44,68 @@ export function createVueNode(
         defineComponent({
           props: nodeViewProps,
           setup(props) {
+            const { editor, node } = props
+            const { state, view } = editor
+            const wrapperRef = ref<typeof NodeViewWrapper | null>(null) // Store wrapper reference
+
+            const onSelectionUpdate = () => {
+              const selection = state.selection
+              const { anchor } = selection
+              const wrapperEl = wrapperRef.value?.$el
+              const contentEl = wrapperEl?.querySelector('[data-node-view-content]')
+              if (!wrapperEl || !contentEl) return
+
+              const resolvedPos = state.doc.resolve(anchor)
+              const startPosition = resolvedPos.start(resolvedPos.depth)
+              const selectionEl = view.nodeDOM(anchor) || view.nodeDOM(startPosition - 1)
+
+              // if the selectionEl is not the contentEl and not inside the contentEl, disable editing
+              if (!(selectionEl === contentEl || contentEl.contains(selectionEl))) {
+                editor.off('selectionUpdate', onSelectionUpdate) // Remove the listener
+                wrapperEl.setAttribute('contenteditable', 'false')
+              }
+            }
+
+            onUnmounted(() => {
+              editor.off('selectionUpdate')
+            })
+
             return () =>
-              // tabindex='-1' is required to make the none-editable node focusable for copying text
               h(
                 NodeViewWrapper,
-                { contenteditable: false, tabindex: '-1' },
+                {
+                  contenteditable: false,
+                  // tabindex='-1' is required to make the none-editable node focusable for copying text
+                  tabindex: '-1',
+                  ref: wrapperRef,
+                  onFocusin: (event: FocusEvent) => {
+                    const target = event.target
+                    const wrapper = wrapperRef.value?.$el
+                    const content = wrapper?.querySelector('[data-node-view-content]')
+                    // If the focus was inside NodeViewContent, enable editing
+                    if (wrapper && content && content.contains(target)) {
+                      // This transfers control to the editor so selectionUpdate event in setup is used to reset on
+                      // focus out of the NodeView. Focus out of content - yet still in nodeView - is handled in onFocusin still
+                      wrapper.setAttribute('contenteditable', 'true')
+                      editor.on('selectionUpdate', onSelectionUpdate) // Attach listener
+                    } else {
+                      wrapper.setAttribute('contenteditable', 'false')
+                      editor.off('selectionUpdate', onSelectionUpdate) // Remove the listener
+                    }
+                  }
+                },
                 {
                   default: () => [
                     h(
                       component,
-                      { ...props.node.attrs },
+                      { ...node.attrs },
                       {
                         default: () =>
                           h(NodeViewContent, {
                             ...(contentAs ? { as: contentAs } : {}),
-                            contenteditable
+                            contenteditable,
+                            // makes sure the onfocus target is the content if clicked when contenteditable is already true
+                            tabindex: '-1'
                           })
                       }
                     )
@@ -70,7 +117,7 @@ export function createVueNode(
         {
           // stopEvent: (event) => {
           //   console.log('stopEvent: ', event)
-          //   return true
+          //   return false
           // }
         }
       )
