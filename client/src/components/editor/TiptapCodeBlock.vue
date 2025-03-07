073f6c9bf22bd71b769ee5d036b22721463bec85
fallenfreq <michaelhigginson@hotmail.co.uk>
2025-03-07 11:51:40 +0000

    Disable langauge selection for code view

--- client/src/components/editor/TiptapCodeBlock.vue
+++ client/src/components/editor/TiptapCodeBlock.vue
@@ -1,7 +1,12 @@
 <template>
   <NodeViewWrapper contenteditable="true" class="code-block">
     <div class="flex b bg-backgroundSecondary p-2 border-b justify-end rounded-t-lg">
-      <select class="bg-transparent" contenteditable="false" v-model="selectedLanguage">
+      <select
+        class="bg-transparent"
+        contenteditable="false"
+        v-model="selectedLanguage"
+        :disabled="editorStore.isCodeView"
+      >
         <option :value="null">Auto</option>
         <option disabled>—</option>
         <option v-for="(language, index) in languages" :value="language" :key="index">
@@ -14,11 +19,13 @@
 </template>
 
 <script setup lang="ts">
-import { NodeViewContent, nodeViewProps, NodeViewWrapper } from '@tiptap/vue-3'
+import { NodeViewContent, type NodeViewProps, NodeViewWrapper } from '@tiptap/vue-3'
 import { computed } from 'vue'
 import highlight from 'highlight.js'
+import { useEditorStore } from '@/stores/editorStore'
+const editorStore = useEditorStore()
 
-const props = defineProps(nodeViewProps)
+const props = defineProps<NodeViewProps>()
 const languages: string[] = props.extension.options.lowlight.listLanguages()
 const languagesName = languages.map((language) => {
   return highlight.getLanguage(language)?.name?.split(',')[0]
