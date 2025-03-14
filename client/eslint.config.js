a7b38b2f686858ef646a15de659fffc26ca8d281
fallenfreq <michaelhigginson@hotmail.co.uk>
2025-03-14 11:52:50 +0000

    TSX support to ESLint

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -1,4 +1,16 @@
 import vueEslintPlugin from 'eslint-plugin-vue'
 import base from '../eslint.config.js'
 
-export default [...base, ...vueEslintPlugin.configs['flat/essential']]
+export default [
+  ...base,
+  {
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true // Ensure TSX parsing is supported
+        }
+      }
+    }
+  },
+  ...vueEslintPlugin.configs['flat/essential']
+]
