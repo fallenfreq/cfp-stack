b6f33bd5f1cf816676706a3fff5e26110d741fc4
fallenfreq <michaelhigginson@hotmail.co.uk>
2025-05-26 18:18:23 +0100

    Update ESLint and Prettier packages and configs

--- eslint.config.js
+++ eslint.config.js
@@ -1,26 +1,50 @@
 import js from '@eslint/js'
+import json from '@eslint/json'
+import markdown from '@eslint/markdown'
+import stylistic from '@stylistic/eslint-plugin'
+import { defineConfig } from 'eslint/config'
+import globals from 'globals'
 import tseslint from 'typescript-eslint'
-import eslintConfigPrettier from 'eslint-config-prettier'
 
-export default [
-  js.configs.recommended,
-  ...tseslint.configs.recommended,
+export default defineConfig([
   {
-    plugins: {
-      'typescript-eslint': tseslint.plugin
-    },
-    rules: {
-      'no-unused-vars': 'off',
-      'no-undef': 'off',
-      'no-console': 'off',
-      '@typescript-eslint/no-explicit-any': 'off'
-    },
-    languageOptions: {
-      parserOptions: {
-        parser: tseslint.parser,
-        sourceType: 'module'
-      }
-    }
+    files: ['**/*.{js,mjs,cjs,ts}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: { globals: globals.node }
   },
-  eslintConfigPrettier
-]
+  tseslint.configs.strict,
+  tseslint.configs.stylistic,
+  {
+    files: ['**/*.json'],
+    ignores: ['tsconfig.*json'],
+    plugins: { json },
+    language: 'json/json',
+    extends: ['json/recommended']
+  },
+  {
+    files: ['**/*.jsonc', 'tsconfig.*json', '.vscode/settings.json'],
+    plugins: { json },
+    language: 'json/jsonc',
+    extends: ['json/recommended']
+  },
+  {
+    files: ['**/*.md'],
+    plugins: { markdown },
+    language: 'markdown/commonmark',
+    extends: ['markdown/recommended']
+  },
+  // This can be used isolated to add individual rules over prettier
+  {
+    files: ['**/*.{js,mjs,cjs,ts}'],
+    plugins: {
+      '@stylistic': stylistic
+    },
+    languageOptions: { parser: tseslint.parser, sourceType: 'module', globals: globals.node },
+    ignores: ['**/dist'],
+    rules: {
+      '@stylistic/operator-linebreak': ['error', 'before'],
+      '@typescript-eslint/no-explicit-any': 'off'
+    }
+  }
+])
---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+const config = {
+  semi: false,
+  tabWidth: 2,
+  singleQuote: true,
+  printWidth: 100,
+  trailingComma: 'none',
+  plugins: ['prettier-plugin-organize-imports']
+}
+
+export default config
