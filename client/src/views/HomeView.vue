b9e1ecac412e2190061881bead78c3f9f4179817
fallenfreq <michaelhigginson@hotmail.co.uk>
2025-05-26 18:07:23 +0100

    Fix typographic inconsistencies in HomeView.vue

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -9,8 +9,8 @@
     </h2>
 
     <p class="text-lg mb-8" style="opacity: 0.8">
-      At somefreq, I specialise in custom web development and strategic branding. Whether you’re
-      looking for ongoing support or a one-time project, I’m here to provide personalised solutions
+      At somefreq, I specialise in custom web development and strategic branding. Whether you're
+      looking for ongoing support or a one-time project, I'm here to provide personalised solutions
       that help your brand stand out. With a strong blend of technical expertise and creative
       design, I work closely with you to turn your bright ideas into reality.
     </p>
@@ -18,16 +18,16 @@
     <h2 class="text-2xl font-semibold mb-6">About Michael</h2>
 
     <p class="text-lg mb-8" style="opacity: 0.8">
-      Hi, I’m Michael, the sole developer and creative force behind somefreq. With nearly 20 years
-      of experience in graphic design and programming, I’ve had the opportunity to work on a wide
+      Hi, I'm Michael, the sole developer and creative force behind somefreq. With nearly 20 years
+      of experience in graphic design and programming, I've had the opportunity to work on a wide
       range of projects that blend creativity with technical expertise. I began my career as a
       graphic designer, specializing in branding and teaching classes for SMEs and personal brands.
       I then moved on to designing and coding emails for Studio.co.uk before co-running an
       internationally renowned photography business for almost a decade, where I wrote and
-      maintained various apps for the business. As a polyglot programmer, I’m comfortable working
-      with multiple languages, always choosing the best tools for the job. Currently, I’m pursuing a
-      Master’s in Computer Science to further hone my skills. Whether it’s a blog, e-commerce site,
-      portfolio, or a complex web app like a SaaS product, I’m excited to help bring your ideas to
+      maintained various apps for the business. As a polyglot programmer, I'm comfortable working
+      with multiple languages, always choosing the best tools for the job. Currently, I'm pursuing a
+      Master's in Computer Science to further hone my skills. Whether it's a blog, e-commerce site,
+      portfolio, or a complex web app like a SaaS product, I'm excited to help bring your ideas to
       life.
     </p>
 
@@ -76,7 +76,7 @@
         <p class="text-lg italic mb-2" style="opacity: 0.9">Building Brands from the Ground Up</p>
         <p class="text-lg" style="opacity: 0.8">
           My approach to branding goes beyond just the visual elements. I help you define your
-          brand’s voice, core values, and overall identity. Whether you’re a start-up looking to
+          brand's voice, core values, and overall identity. Whether you're a start-up looking to
           establish your presence or an individual crafting a personal brand, I offer end-to-end
           consultation services to make sure your brand resonates with your target audience.
         </p>
